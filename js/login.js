14f2d7f87d5837606edc55a2ea8c318789a2743c
Juan Estrada <100156819+david24748@users.noreply.github.com>
2023-11-30 01:38:30 -0500

    project finished

--- js/login.js
+++ js/login.js
@@ -4,9 +4,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if(username === 'Juli' && password === 'Teamo') {
+    if(username === 'Te amo' && password === 'feliz aniversario') {
         document.getElementById('message').textContent = '¡Inicio de sesión exitoso!';
-        window.location.href = 'index.html';
+        window.location.href = 'principal.html';
     } else {
         document.getElementById('message').textContent = 'Nombre de usuario o contraseña incorrectos.';
     }
--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,4 @@
 document.getElementById('logoutButton').addEventListener('click', function() {
     localStorage.clear();
-    window.location.href = 'login.html';
+    window.location.href = 'index.html';
 });
